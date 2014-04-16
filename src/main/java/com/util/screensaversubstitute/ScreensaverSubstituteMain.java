cea3ae0b462200a49cf11fa9e9021b05f2bfd3c6
brettRoser <brett.roser@yahoo.com>
2014-04-16 00:08:36 -0500

    Initial Commit... very first version of this program

---
+++ src/main/java/com/util/screensaversubstitute/ScreensaverSubstituteMain.java
@@ -0,0 +1,11 @@
+package screensaversubstitute;
+
+public class ScreensaverSubstituteMain 
+{
+  public static void main(String[] args) {
+    ScreensaverSubstituteGui gui = new ScreensaverSubstituteGui();
+    gui.setVisible(true);
+    gui.setResizable(false);
+  } // end of main method
+} // end of class
+
